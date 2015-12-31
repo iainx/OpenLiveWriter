3d4de158211fa93bea2b65c630219631799c9d79
iain holmes <iain@xamarin.com>
2015-12-31 13:58:07 +0000

    [MacPlatform] Initial Mac platform project

---
+++ src/managed/OpenLiveWriter.MacPlatform/MacPlatform.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace OpenLiveWrite.MacPlatform
+{
+    public class MacPlatform
+    {
+        public MacPlatform ()
+        {
+            Console.WriteLine ("Mac Platform");
+        }
+    }
+}
+
